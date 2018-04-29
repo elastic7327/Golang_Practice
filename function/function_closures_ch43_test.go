63cf1d911a90a1ca05d2dfab25f2d8b4b9fd1b82
dan <elastic7327@gmail.com>
2018-04-30 00:35:42 +0900

    add practice

---
+++ function/function_closures_ch43_test.go
@@ -0,0 +1,37 @@
+package function
+
+import (
+	"fmt"
+	"testing"
+)
+
+func adder() func(int) int {
+	sum := 0
+
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
+func TestFunctionClosure(t *testing.T) {
+	/*
+
+		그리고 함수는 클로져(full closures) 입니다.
+
+		코드에서 adder 함수는 클로져(closure)를 반환합니다.
+
+		각각의 클로져는 자신만의 sum 변수를 가집니다.
+
+	*/
+
+	pos, neg := adder(), adder()
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
+
+}
---
+++ function/function_fibonacci_ch44_test.go
@@ -0,0 +1,34 @@
+package function
+
+import (
+	"fmt"
+	"testing"
+)
+
+func fib() func() int {
+
+	a, b := 0, 1
+
+	return func() int {
+
+		a, b = b, a+b
+
+		return a
+	}
+
+}
+
+func TestFibonacci(t *testing.T) {
+	/*
+		함수를 가지고 놀아봅시다.
+
+		fibonacci 함수를 구현합니다. 이 함수는 이어지는 피보나치 수를 반환하는 함수 (클로져)를 반환해야 합니다.
+	*/
+
+	f := fib()
+
+	for i := 0; i < 100; i++ {
+		fmt.Println(f())
+	}
+
+}
